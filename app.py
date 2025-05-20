dfcaf1a1584cc3bb5b89d5beaa7e7160501af947
Fredy Ramadhan <fredytri.ramadhan@gmail.com>
2025-05-20 12:39:19 +0700

    bikin proyek flask

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, render_template
+# from fitur_fitur.cek_equivalen import
+# from fitur_fitur.minimisasi_dfa import 
+# from fitur_fitur.regex_to_nfa import 
+# from fitur_fitur.tes_dfa import 
 
 app = Flask(__name__)
 
